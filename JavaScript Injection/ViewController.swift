9fac31f2a02afa4fe54879d6c2a9d1665f93dd81
Michele Galvagno <michicello@gmail.com>
2019-04-17 09:27:56 +0200

    Initial Commit

---
+++ JavaScript Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JavaScript Injection
+//
+//  Created by Michele Galvagno on 17/04/2019.
+//  Copyright © 2019 Michele Galvagno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
