3a17ef6c311576f1e1d66664b5ec571708562f0f
Michele Galvagno <michicello@gmail.com>
2019-04-18 23:47:15 +0200

    Challenge 2 copied
    This was copied from this repository: https://github.com/nocto7/Project19/blob/master/Extension/ActionViewController.swift
    
    It saves but it doesn't load ...

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -15,12 +15,24 @@ class ActionViewController: UIViewController {
     
     var pageTitle = ""
     var pageURL = ""
+    
+    var userDefaults: UserDefaults!
+    var savedScripts: [String: String] = [:]
+    var keyName = "customJSExtension"
 
     // View management
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userDefaults = UserDefaults.standard
+        savedScripts = userDefaults.dictionary(forKey: keyName) as? [String: String] ?? [:]
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        let chooseButton = UIBarButtonItem(title: "Choose", style: .plain, target: self, action: #selector(chooseScript))
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
+        let loadButton = UIBarButtonItem(title: "Load", style: .plain, target: self, action: #selector(load))
+        navigationItem.leftBarButtonItems = [saveButton, loadButton, chooseButton]
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -36,6 +48,14 @@ class ActionViewController: UIViewController {
                     
                     DispatchQueue.main.async {
                         self?.title = self?.pageTitle
+                        if let hostName = URL(string: self!.pageURL)?.host! {
+                            let hostKey = "[autosaved from] " + hostName
+                            let obj = self?.savedScripts[hostKey, default: "[Type your javascript in here]"]
+                            
+                            if let scriptText = obj {
+                                self?.script.text = scriptText
+                            }
+                        }
                     }
                 }
             }
@@ -51,6 +71,72 @@ class ActionViewController: UIViewController {
         item.attachments = [customJavaScript]
         
         extensionContext?.completeRequest(returningItems: [item])
+        
+        // save settings if we have a host
+        if let hostName = URL(string: pageURL)?.host! {
+            let hostKey = "[autosaved from] " + hostName
+            savedScripts[hostKey] = script.text
+            userDefaults.set(savedScripts, forKey: keyName)
+        }
+    }
+    
+    @objc func save() {
+        let saveAC = UIAlertController(title: "Name your script", message: nil, preferredStyle: .alert)
+        saveAC.addTextField()
+        saveAC.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+            if let saveName = saveAC.textFields![0].text {
+                self.savedScripts[saveName] = self.script.text
+                self.userDefaults.set(self.savedScripts, forKey: self.keyName)
+                self.userDefaults.set(self.script.text, forKey: saveName)
+            }
+        })
+        
+        present(saveAC, animated: true)
+    }
+    
+    @objc func load() {
+        
+    }
+    
+    @objc func chooseScript() {
+        let ac = UIAlertController(title: "Choose Script", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Page Title", style: .default, handler: { _ in
+            self.script.text = "alert(document.title);"
+        }))
+        ac.addAction(UIAlertAction(title: "Count Links", style: .default, handler: { _ in
+            self.script.text = """
+            var links = document.getElementsByTagName("a");
+            alert("There are " + links.length + " links on this page.");
+            """
+        }))
+        ac.addAction(UIAlertAction(title: "Just the Headlines", style: .default, handler: { _ in
+            self.script.text = """
+            for (var k=1; k<7; k++) {
+            var headlines = document.getElementsByTagName("h" + k);
+            var allHeadlines = ""
+            for(var i=0; i<headlines.length; i++) {
+            allHeadlines += headlines[i].innerText + " / ";
+            }
+            if (i>0) {
+            alert("There are " + i + " level " + k + " headlines: " + allHeadlines);}
+            }
+            """
+        }))
+        ac.addAction(UIAlertAction(title: "Hacked with Swift", style: .default, handler: { _ in
+            self.script.text = """
+            var level = prompt("Which headlines shall we change (choose from 1 to 6)?")
+            var newHeadline = prompt("And what shall we change them to?")
+            
+            var headlines = document.getElementsByTagName("h" + level);
+            
+            for(var i=0; i<headlines.length; i++) {
+            headlines[i].innerText = newHeadline
+            }
+            """
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
     
     // Manage the keyboard state change
